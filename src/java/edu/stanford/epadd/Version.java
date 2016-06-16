7a2295f266c548931e94c792b483cc6848f5504e
Sudheendra Hangal <hangal@cs.stanford.edu>
2016-06-16 09:23:21 +0530

    added resources readme.

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "June 8, 2016";
-public static final String buildInfo = "Built by hangal at Jun 13, 2016 07:15 PM on Mac OS X 10.11.2 x86_64 ";
+public static final String buildInfo = "Built by hangal at Jun 15, 2016 02:16 PM on Mac OS X 10.11.2 x86_64 ";
 }
