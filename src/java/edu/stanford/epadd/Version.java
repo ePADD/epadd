3782f63b37873b9bfb41f47ca14aed8fe673e75e
Sudheendra Hangal <hangal@cs.stanford.edu>
2016-08-19 12:34:26 -0700

    fixed export mbox to look at cart in delivery mode, otherwise dump all messages.

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 2";
-public static final String buildInfo = "Built by hangal at Aug 19, 2016 10:45 AM on Mac OS X 10.11.2 x86_64 ";
+public static final String buildInfo = "Built by hangal at Aug 19, 2016 12:21 PM on Mac OS X 10.11.2 x86_64 ";
 }
