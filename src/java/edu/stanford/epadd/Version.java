e0c188cfaa18d337ebfc308bbfb1aed1c361810f
Sudheendra Hangal <hangal@gmail.com>
2017-06-30 19:40:28 +0530

    added regex lex

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 4.0-alpha (June 30, 2017)";
-public static final String buildInfo = "Built by hangal at Jun 30, 2017 07:16 PM on Mac OS X 10.12.5 x86_64 ";
+public static final String buildInfo = "Built by hangal at Jun 30, 2017 07:28 PM on Mac OS X 10.12.5 x86_64 ";
 }
