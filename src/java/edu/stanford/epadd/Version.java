b03622032838dbce37ea394243882b2b2368c24d
hangal <hangal@stanfordalumni.org>
2017-07-09 19:26:28 +0530

    pulled

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 4.0-alpha (June 30, 2017)";
-public static final String buildInfo = "Built by hangal at Jun 30, 2017 07:48 PM on Mac OS X 10.12.5 x86_64 ";
+public static final String buildInfo = "Built by hangal at Jul 09, 2017 06:59 PM on Mac OS X 10.12.1 x86_64 ";
 }
