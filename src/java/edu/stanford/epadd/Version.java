14663136cfcab9bfe9f7568b2fb054d2fc5c5a11
Sudheendra Hangal <hangal@gmail.com>
2017-07-17 12:19:57 +0530

    new icons

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
-public class Version {  public static final String version = "Release 4.0-alpha (June 30, 2017)";
-public static final String buildInfo = "Built by hangal at Jun 30, 2017 07:48 PM on Mac OS X 10.12.5 x86_64 ";
+public class Version {  public static final String version = "Release 4.0-beta1 (July 6, 2017)";
+public static final String buildInfo = "Built by hangal at Jul 07, 2017 09:57 PM on Mac OS X 10.12.5 x86_64 ";
 }
