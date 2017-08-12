19599b79a6ab38ff65136171043fb28994f50251
Chinmay Narayan <chinu.pandey@gmail.com>
2017-08-12 13:06:03 +0530

    Build info

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 4.0-beta1 (July 6, 2017)";
-public static final String buildInfo = "Built by hangal at Jul 17, 2017 12:29 PM on Mac OS X 10.12.5 x86_64 ";
+public static final String buildInfo = "Built by chinmay at Aug 11, 2017 02:46 PM on Linux 4.10.0-28-generic amd64 ";
 }
