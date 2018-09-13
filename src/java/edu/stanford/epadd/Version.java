7be421c91c64f2bd70c5a29e67738d18ccb60710
Administrator <chinu.pandey@gmail.com>
2018-09-13 08:18:16 +0530

    update release version

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 6.0 (July 31, 2018)";
-public static final String buildInfo = "Built by tech at Sep 12, 2018 10:38 PM on Mac OS X 10.13.6 x86_64 ";
+public static final String buildInfo = "Built by tech at Sep 13, 2018 08:08 AM on Mac OS X 10.13.6 x86_64 ";
 }
