87c69038b9da8d2f5408b0dd2b81a7b17b3dd1cd
Administrator <chinu.pandey@gmail.com>
2018-10-16 11:50:07 +0530

    Fix for issue #327

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 7 beta-1 (Oct 15, 2018)";
-public static final String buildInfo = "Built by tech at Oct 15, 2018 06:38 PM on Mac OS X 10.13.6 x86_64 ";
+public static final String buildInfo = "Built by tech at Oct 15, 2018 07:24 PM on Mac OS X 10.13.6 x86_64 ";
 }
