c4030e949e5610213ac67cb1fa2b63439a1db401
Administrator <chinu.pandey@gmail.com>
2019-05-07 21:33:57 +0530

    version file

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 7.0";
-public static final String buildInfo = "Built by tech at Jan 30, 2019 12:53 PM on Mac OS X 10.14 x86_64 ";
+public static final String buildInfo = "Built by tech at Apr 29, 2019 02:57 PM on Mac OS X 10.14 x86_64 ";
 }
