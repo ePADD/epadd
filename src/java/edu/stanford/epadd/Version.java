1301c187dfd5b4520f49844d50b14406ed292965
Administrator <chinu.pandey@gmail.com>
2020-06-13 12:39:35 +0530

    version

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 7.3";
-public static final String buildInfo = "Built by tech at May 27, 2020 08:56 PM on Mac OS X 10.15 x86_64 ";
+public static final String buildInfo = "Built by tech at Jun 12, 2020 04:35 PM on Mac OS X 10.15 x86_64 ";
 }
