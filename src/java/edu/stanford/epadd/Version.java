f00a94b800dd9deef0fadb39963f06b38dfb7e69
hangal <hangal@stanfordalumni.org>
2020-08-10 13:15:59 +0530

    updated version

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
 public class Version {  public static final String version = "Release 7.3";
-public static final String buildInfo = "Built by tech at Jun 12, 2020 04:35 PM on Mac OS X 10.15 x86_64 ";
+public static final String buildInfo = "Built by hangal at Jul 16, 2020 08:25 PM on Mac OS X 10.15.5 x86_64 ";
 }
