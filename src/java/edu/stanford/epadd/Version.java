8512e019ed2e085381f7fe4daa6c6ed8d105164d
Jochen <jochen.farwer@manchester.ac.uk>
2026-03-27 18:08:01 +0000

    Setting version number

--- src/java/edu/stanford/epadd/Version.java
+++ src/java/edu/stanford/epadd/Version.java
@@ -1,4 +1,4 @@
 package edu.stanford.epadd;
-public class Version {  public static final String version = "Release";
+public class Version {  public static final String version = "Release 11.1.1-alpha";
 public static final String buildInfo = "";
 }
